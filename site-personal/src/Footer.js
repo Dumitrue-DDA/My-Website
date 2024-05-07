1f43895779c68d87f9bb6002941ab4c901577830
Dumitru-AD <despina.andrei2003@gmail.com>
2024-05-07 18:43:14 +0300

    Finished footer and clean the rest of the code
    Finished the footer section and made it responsive. Clean the rest of the code (mostly the styling).

--- site-personal/src/Footer.js
+++ site-personal/src/Footer.js
@@ -3,31 +3,22 @@ function Footer() {
     return (
         <footer>
             <div className="footer-left">
-                <p>
-                    Last modified 6 May 2024
-                </p>
-                <hr style={{marginRight: 50}}/>
-                <p>
-                    The purpose of this website is for me to learn to use the React library  
-                </p>
-            </div>
+                <p> Last modified: 7th of May 2024
+                    <hr />
+                    The purpose of this website is for me to learn to use the React library </p> </div>
             <div className="footer-right">
-                <p>
-                        Contact information:<br />
-                    <span><img src={require("./assets/email.png")} alt={"email-icon"}/>
+                       <p> Contact information:<hr />
+                    <span> <img src={require("./assets/email.png")} alt={"email-icon"}/>
                         Email: despina.andrei2003@gmail.com
                     </span>
-                    <span><img src={require("./assets/telephone.png")} alt={"phone-icon"}/>
+                    <span> <img src={require("./assets/telephone.png")} alt={"phone-icon"}/>
                         Phone number: (+40)737 631 851
                     </span>
-                    <span><img src={require("./assets/linkedin.png")} alt={"linkedin-icon"}/> Linkedin:
-                    <a href="https://www.linkedin.com/in/dumitru-andrei-despina/">
+                    <span> <img src={require("./assets/linkedin.png")} alt={"linkedin-icon"}/>
+                    Linkedin: <a href="https://www.linkedin.com/in/dumitru-andrei-despina/">
                         www.linkedin.com/in/dumitru-andrei-despina
-                    </a></span>
-                </p>
-            </div>
+                    </a> </span> </p> </div>
         </footer>
     );
 }
-
 export default Footer
